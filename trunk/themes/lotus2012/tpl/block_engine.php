59c2469641a32da3ddbfdb5d0e8de5261d37dd09
youtube@lotus-online.de <youtube@lotus-online.de@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-01-30 16:39:47 +0000

    commit the newest version of the theme

--- trunk/themes/lotus2012/tpl/block_engine.php
+++ trunk/themes/lotus2012/tpl/block_engine.php
@@ -1,25 +1,22 @@
-			<!--
-				<{php }>
-					$block_id = 3; 
-					$theme = $GLOBALS['icmsConfig']['theme_set']; 
-					include XOOPS_THEME_PATH.'/'.$theme.'/tpl/block_engine.php';
-				<{/php}>
-				<{$block.content}>			-->		
-
+		
 <?php
-	$icmsobject = new icms_view_block_Object($block_id);
-	$template = new icms_view_Tpl();
-	$block = array(
-		'id'        => $icmsobject->getVar( 'bid' ),
-		'module'    => $icmsobject->getVar( 'dirname' ),
-		'title'     => $icmsobject->getVar( 'title' ),
-		'weight'    => $icmsobject->getVar( 'weight' ),
-		'lastmod'   => $icmsobject->getVar( 'last_modified' ),
-	);
-	$tplName = ( $tplName = $icmsobject->getVar('template') ) ? "db:$tplName" : "db:system_block_dummy.html";	
-	$bresult = $icmsobject->buildBlock() ; 
- 		$template->assign( 'block', $bresult );
-		$block['content'] = $template->fetch($tplName);
-	
-	 $this->assign('block', $block);	
+$block_handler = xoops_gethandler('block');
+$xobject = $block_handler->get($block_id, true);
+
+$template = new icms_view_Tpl();
+$block = array(
+'id'        => $xobject->getVar( 'bid' ),
+'module'    => $xobject->getVar( 'dirname' ),
+'title'     => $xobject->getVar( 'title' ),
+'weight'    => $xobject->getVar( 'weight' ),
+'lastmod'   => $xobject->getVar( 'last_modified' ),
+);
+$tplName = ( $tplName = $xobject->getVar('template') ) ? "db:$tplName" : "db:system_block_dummy.html";
+if ( $bresult = $xobject->buildBlock() ) {
+	$template->assign( 'block', $bresult );
+	$block['content'] = $template->fetch($tplName);
+} else {
+	$block = false;
+}
+$this->assign('block', $block);
 ?>
