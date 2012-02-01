578b8b8b79a31fa74bf90cd88f703bbd5b780ccc
impresscms <impresscms@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-02-01 02:03:46 +0000

    Missed language variables added and style for font-size changed. The jQuery UI will take the font-size from the theme now.

--- visitorvoice/language/english/main.php
+++ visitorvoice/language/english/main.php
@@ -21,3 +21,5 @@ define("_MD_VISITORVOICE_PUBLISHED_BY", "Published by");
 define("_MD_VISITORVOICE_PUBLISHED_ON", "Published on");
 define("_MD_VISITORVOICE_PERM", "Sorry, no permissions");
 define("_MD_VISITORVOICE_PERMISSION", "You currently have no permissions. Please login or register to submit an entry.");
+define("_MD_VISITORVOICE_CREATED", "Visitorvoice entry created, thank you.");
+define("_MD_VISITORVOICE_MODIFIED", "Visitorvoice entry ,modified.");
