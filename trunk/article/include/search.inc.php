4b76149fbef20bfb23edcb61ec0b8621c96ada84
St.Flohrer@gmail.com <St.Flohrer@gmail.com@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-02-03 19:00:58 +0000

    fixed search

--- trunk/article/include/search.inc.php
+++ trunk/article/include/search.inc.php
@@ -13,7 +13,7 @@
  */
 
 defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
-
+include_once ICMS_ROOT_PATH . '/modules/article/include/common.php';
 function article_search($queryarray, $andor, $limit, $offset, $userid) {
 	$article_article_handler = icms_getModuleHandler('article', basename(dirname(dirname(__FILE__))), 'article');
 	$articleArray = $article_article_handler->getArticlesForSearch($queryarray, $andor, $limit, $offset, $userid);
@@ -21,7 +21,7 @@ function article_search($queryarray, $andor, $limit, $offset, $userid) {
 	$ret = array();
 
 	foreach ($articleArray as $articleArray) {
-		$item['image'] = "images/article_icon.png";
+		$item['image'] = "images/article_icon_big.png";
 		$item['link'] = $articleArray['itemURL'];
 		$item['title'] = $articleArray['article_title'];
 		$item['time'] = strtotime($articleArray['article_published_date']);
