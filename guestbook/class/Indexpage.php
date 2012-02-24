006828a968b5ee8100eaf06d93bdc9ceb22bb3b6
st.flohrer <St.Flohrer@gmail.com>
2012-02-24 16:28:33 +0000

    fixed issue for img-upload via url

--- guestbook/class/Indexpage.php
+++ guestbook/class/Indexpage.php
@@ -36,7 +36,7 @@ class GuestbookIndexpage extends icms_ipf_Object {
 		$this->initCommonVar("dosmiley", FALSE, 1);
 		$this->initCommonVar("docxode", FALSE, FALSE, FALSE, 1);
 
-		$this->setControl('index_img_upload', 'image');
+		$this->setControl('index_img_upload', 'imageupload');
 		$this->setControl('index_heading','dhtmltextarea');
 		$this->setControl('index_footer', array('name' => 'textarea', 'form_editor' => 'htmlarea'));
 		$this->setControl('index_image', array( 'name' => 'select', 'itemHandler' => 'indexpage', 'method' => 'getImageList', 'module' => 'guestbook'));
