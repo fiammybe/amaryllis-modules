8fe1bef2b53e327a01faf4ebb282850441dbd993
St.Flohrer@gmail.com <St.Flohrer@gmail.com@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-03-08 11:36:28 +0000

    ongoing dev process

--- trunk/visitorvoice/class/IndexpageHandler.php
+++ trunk/visitorvoice/class/IndexpageHandler.php
@@ -32,22 +32,11 @@ class VisitorvoiceIndexpageHandler extends icms_ipf_Handler {
 	 */
 	public function __construct(&$db) {
 		parent::__construct($db, "indexpage", "index_key", "index_header", "index_heading", "visitorvoice");
-
-		$this->_uploadPath = ICMS_ROOT_PATH . '/uploads/' . basename(dirname(dirname(__FILE__))) . '/indexpage';
 		$mimetypes = array('image/jpeg', 'image/png', 'image/gif');
 		$this->enableUpload($mimetypes, 2000000, 900, 900);
 		
 	}
-	
-	public function getImagePath() {
-		$dir = $this->_uploadPath;
-		if (!file_exists($dir)) {
-			$moddir = basename(dirname(dirname(__FILE__)));
-			icms_core_Filesystem::mkdir($dir, "0777", ICMS_ROOT_PATH . '/uploads/' . $moddir . '/' );
-		}
-		return $dir . "/";
-	}
-	
+
 	static public function getImageList() {
 		$indeximages = array();
 		$indeximages = icms_core_Filesystem::getFileList(VISITORVOICE_UPLOAD_ROOT . 'indeximages/', '', array('gif', 'jpg', 'png'));
