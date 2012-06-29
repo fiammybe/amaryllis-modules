12a097c36673e537d8623559e9b12a06bfe17030
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-06-29 08:50:28 +0000

    test issue#64 test issue#65 did the same changes again, there seems to be some trouble while switching the workplace so I lost the changes

--- trunk/icmspoll/submitpoll.php
+++ trunk/icmspoll/submitpoll.php
@@ -37,6 +37,8 @@ if(in_array($clean_op, $valid_op, TRUE)) {
 				$vote = $pollObj->vote($_POST['poll_option'], xoops_getenv('REMOTE_ADDR'), $clean_uid);
 				if($vote) redirect_header(icms_getPreviousPage(), 4, _MD_ICMSPOLL_POLLS_THANKS_VOTING);
 				redirect_header(icms_getPreviousPage(), 4, _MD_ICMSPOLL_POLLS_NOTVOTED);
+			} else {
+				redirect_header(icms_getPreviousPage(), 4, _NOPERM);
 			}
 			break;
 	}
