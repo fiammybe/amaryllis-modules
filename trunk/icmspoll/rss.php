b40ccb5373a8194668c99e199710bfea8932f610
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-06-30 20:37:34 +0000

    fiyed typos

--- trunk/icmspoll/rss.php
+++ trunk/icmspoll/rss.php
@@ -38,12 +38,12 @@ if($icmspollConfig['use_rss'] == 1) {
 	
 	foreach($postsArray as $postArray) {
 		$poll_feed->feeds[] = array (
-		  'title' => $postArray['title'],
-		  'link' => str_replace('&', '&amp;', $postArray['itemURL']),
-		  'author' => $postArray['user'],
+		  'title' 		=> $postArray['question'],
+		  'link' 		=> str_replace('&', '&amp;', $postArray['itemURL']),
+		  'author' 		=> $postArray['user'],
 		  'description' => icms_core_DataFilter::htmlSpecialChars(str_replace('&', '&amp;', $postArray['dsc']), ENT_QUOTES, _CHARSET),
-		  'pubdate' => $postArray['created_on'],
-		  'guid' => str_replace('&', '&amp;', $postArray['itemURL']),
+		  'pubdate' 	=> $postArray['start_time'],
+		  'guid' 		=> str_replace('&', '&amp;', $postArray['itemURL']),
 		);
 	}
 	$poll_feed->render();
