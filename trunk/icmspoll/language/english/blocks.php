58e7c985d8854228af87290e4fa26e49b2d66473
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-06-30 08:20:10 +0000

    adjusted templates

--- trunk/icmspoll/language/english/blocks.php
+++ trunk/icmspoll/language/english/blocks.php
@@ -27,4 +27,15 @@ define("_MB_ICMSPOLL_BLOCK_ORDER", "Order by");
 define("_MB_ICMSPOLL_BLOCK_SELPOLL", "Select poll to be displayed");
 
 define("_MB_ICMSPOLL_POLLS_ALREADYVOTED", "Already voted");
-define("_MB_ICMSPOLL_POLLS_SUBMIT", "Submit");
+define("_MB_ICMSPOLL_POLLS_SUBMIT", "Vote now!");
+define("_MB_ICMSPOLL_RESULTS_TOTALVOTES", "Total votes");
+define("_MB_ICMSPOLL_RESULTS_TOTALVOTES_REGISTERED", "Total votes by registred users");
+define("_MB_ICMSPOLL_RESULTS_TOTALVOTES_ANONS", "Total anonymous votes");
+define("_MB_ICMSPOLL_RESULTS_MORE_BY_UNAME", "All results by");
+define("_MB_ICMSPOLL_RESULTS_BY_OPTION_ANON", "Total anonymous votes of this option");
+define("_MB_ICMSPOLL_RESULTS_BY_OPTION_USERS", "Total user votes of this option");
+define("_MB_ICMSPOLL_RESULTS_BY_OPTION_TOTAL", "Total votes of this option");
+define("_MB_ICMSPOLL_PUBLISHER", "Poll has been started by");
+define("_MB_ICMSPOLL_START_ON", "Started on");
+define("_MB_ICMSPOLL_END_ON", "Ended on");
+define("_MB_ICMSPOLL_CREATED_ON", "Created on");
