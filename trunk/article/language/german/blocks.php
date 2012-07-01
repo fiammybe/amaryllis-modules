71213ad0d922dc6a3125d6ee7c2a69fd16cc175d
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-07-01 06:58:15 +0000

    fixed typo, adding german language files to article

---
+++ trunk/article/language/german/blocks.php
@@ -0,0 +1,24 @@
+﻿<?php
+/**
+* English language constants used in blocks of the module
+*
+* @copyright	Copyright QM-B (Steffen Flohrer) 2012
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		QM-B <qm-b@hotmail.de>
+* @package		article
+* @version		$Id$
+*/
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+define("_MB_ARTICLE_ARTICLE_RECENT_LIMIT", "Limit");
+define("_MB_ARTICLE_CATEGORY_CATSELECT", "Wählen Sie eine Kategorie");
+define("_MB_ARTICLE_VIEW_ALL", "Mehr");
+define("_MB_ARTICLE_CATEGORY_SHOWSUBS", "Unterkategorien anzeigen?");
+define("_MB_ARTICLE_CATEGORY_SORT", "Sortiert nach");
+define("_MB_ARTICLE_CATEGORY_ORDER", "Geordnet nach");
+define("_MB_ARTICLE_SHOWTEASER", "Teaser anzeigen?");
+define("_MB_ARTICLE_SHOWMORE", "'Show more'-Link anzeigen?");
+define("_MB_ARTICLE_READ_MORE", "Weiterlesen...");
+define("_MB_ARTICLE_READS", "Gelesen");
---
+++ trunk/article/language/german/mail_template/category_article_modified.tpl
@@ -0,0 +1,16 @@
+Hello {X_UNAME},
+
+You are receiving this message because you selected to be notified when any file in "{CATEGORY_NAME}" category on {X_SITENAME} was modified.
+
+Article ({ARTICLE_TITLE}): {ARTICLE_URL}
+
+If this is an error or you wish not to receive further such notifications, please update your subscriptions by visiting the link below:
+{X_UNSUBSCRIBE_URL}
+
+Please do not reply to this message.
+
+-----------
+
+{X_SITENAME} ({X_SITEURL}) 
+webmaster
+{X_ADMINMAIL}
