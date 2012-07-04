203b4b5bf6e254be11c3df2ad0cc9c61691d95d6
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-07-04 11:47:36 +0000

    - adjusted manual.html - added print support to manual.html - adjusted admin/admin_footer.php to display version info and font size is smaller now - added clearing to print layout

--- trunk/icmspoll/admin/admin_footer.php
+++ trunk/icmspoll/admin/admin_footer.php
@@ -17,8 +17,12 @@
  *
  */
 
-$footer = "<div style='margin: 2em auto; text-align: center; font-size: 1.5em;'>";
-$footer .= "If you need help with the module, please check the <a href='" . ICMSPOLL_ADMIN_URL . "manual.php' title='manual' style='color: #336699;'>manual</a>";
+$version = number_format(icms::$module->getVar('version')/100, 2);
+$version = !substr($version, -1, 1) ? substr($version, 0, 3) : $version;
+
+$footer = "<div style='margin: 2em auto; text-align: center; font-size: 1em;'>";
+$footer .= "If you need help with the module, please check the <a href='manual.php' title='manual' style='color: #336699;'>manual</a>! ";
+$footer .= "&nbsp;Powered by <a href='about.php'>Icmspoll ".$version."</a>";
 $footer .= "</div>";
 
 echo $footer;
