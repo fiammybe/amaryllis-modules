6517bed9020c19602230ce7fadcff6d885c568d5
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-10-23 14:04:42 +0000

    fixed constant, added german language files

---
+++ trunk/portfolio/language/german/blocks.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 'Portfolio' is an portfolio management module for ImpressCMS
+ *
+ * File: /language/german/blocks.php
+ * 
+ * blocks language file
+ * 
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ * 				Portfolio
+ * @since		1.00
+ * @author		QM-B <qm-b@hotmail.de>
+ * @version		$Id$
+ * @package		portfolio
+ *
+ */
+
+defined("ICMS_ROOT_PATH") or die("ImpressCMS Basispfad wurde nicht definiert");
+
+define("_MB_PORTFOLIO_PORTFOLIO_RECENT_LIMIT", "Limit");
+define("_MB_PORTFOLIO_CATEGORY_CATSELECT", "W&auml;hlen Sie eine Kategorie.");
+define("_MB_PORTFOLIO_SORT", "Sortieren");
+define("_MB_PORTFOLIO_ORDER", "Reihenfolge");
+
+
+
+
+
