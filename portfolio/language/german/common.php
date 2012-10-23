a253624cd0ac49db3c5f42ecfb42d4a3033a34c9
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-10-23 14:04:42 +0000

    fixed constant, added german language files

---
+++ portfolio/language/german/common.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * 'Portfolio' is an portfolio management module for ImpressCMS
+ *
+ * File: /language/german/common.php
+ * 
+ * common language file
+ * 
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ * 				Portfolio
+ * @since		1.00
+ * @author		QM-B <qm-b@hotmail.de>
+ * @version		$Id$
+ * @package		portfolio
+ *
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// constants used in portfolio form
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_TITLE", "Title");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_TITLE_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_CID", "Kategorie");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_CID_DSC", "W&auml;hlen Sie eine Kategorien f�r das Referenzobjekt");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_SUMMARY", "Zusammenfassung");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_SUMMARY_DSC", "Die Kurzzusammenfassung wird auf Kategorie Index-Seite verwendet");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_SHOW_SUMMARY", "Zeige die Zusammenfassung im Haupttext auf der Einzel-Seite");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_DESCRIPTION", "Beschreibung");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_DESCRIPTION_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_IMG", "Bild");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_IMG_DSC", "Ein Referenzbild ist erforderlich!");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_ALBUM", "Album");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_ALBUM_DSC", "W&auml;hlen Sie das Album,welches Sie f�r das Referenzobjekt verwende wollen");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_CUSTOMER", "Auftraggeber");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_CUSTOMER_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_URL", "Webseite des Auftraggebers");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_URL_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_P_DATE", "Ver&ouml;ffentlicht");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_P_DATE_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_U_DATE", "Zuletzt aktualisiert am");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_U_DATE_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_SUBMITTER", "Verfasst von");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_SUBMITTER_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_UPDATER", "Zuletzt ge&auml;ndert von");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_UPDATER_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_ACTIVE", "Active?");
+define("_CO_PORTFOLIO_PORTFOLIO_PORTFOLIO_ACTIVE_DSC", "");
+define("_CO_PORTFOLIO_PORTFOLIO_WEIGHT", "Bedeutung");
+define("_CO_PORTFOLIO_PORTFOLIO_WEIGHT_DSC", "");
+
+// constants used in category form
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_TITLE", "Title");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_TITLE_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_LOGO", "Kategorie Bild");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_LOGO_DSC", "W&auml;hlen Sie ein Bild aus. Oder laden Sie ein Neues hoch");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_LOGO_UPL", "Bild-Upload");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_LOGO_UPL_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_DESCRIPTION", "Beschreibung");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_DESCRIPTION_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_SUBMITTER", "Verfasst von");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_SUBMITTER_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_UPDATER", "Zuletzt ge&auml;ndert von");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_UPDATER_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_P_DATE", "Ver&ouml;ffentlicht am");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_P_DATE_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_U_DATE", "Aktualisiert am");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_U_DATE_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_ACTIVE", "Active?");
+define("_CO_PORTFOLIO_CATEGORY_CATEGORY_ACTIVE_DSC", "");
+define("_CO_PORTFOLIO_CATEGORY_WEIGHT", "Bedeutung");
+define("_CO_PORTFOLIO_CATEGORY_WEIGHT_DSC", "");
+
+// constants used for contact form
+define("_CO_PORTFOLIO_CONTACT_CONTACT_TITLE", "Anrede");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_TITLE_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_NAME", "Name");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_NAME_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_MAIL", "E-Mail");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_MAIL_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_PHONE", "Telefon");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_PHONE_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_BODY", "Nachricht");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_BODY_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_SUBMITTER", "Verfasst von");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_SUBMITTER_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_DATE", "&uuml;bermittelt am");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_DATE_DSC", "");
+define("_CO_PORTFOLIO_CONTACT_CONTACT_ISNEW", "Gelesen");
+define("_CO_PORTFOLIO_MESSAGE_REPLY", "Antwort senden");
+define("_CO_PORTFOLIO_CONTACT_UNREAD", "Ungelesene Nachrichten");
+define("_CO_PORTFOLIO_CONTACT_READ", "Gelesene Nachrichten");
+
+// constants for indexpage form
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_HEADER", "&Uuml;berschrift");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_HEADER_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_HEADING", "Beschreibung");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_HEADING_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_FOOTER", "Fusszeile");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_FOOTER_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_IMAGE", "Bild");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_IMAGE_DSC", "W&auml;hlen Sie ein Index Bild aus.Oder laden Sie eine Neues hoch");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_IMG_UPL", "Neues Bild Hochladen");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_IMG_UPL_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_1", "F&auml;higkeit 1");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_1_DSC", "Geben Sie Ihre F&auml;higkeiten. Die F&auml;higkeiten werden wir wie ein F&auml;nf-Sterne-Bewertung verwendet.<br >
+Bitte trennen Sie Ihr Geschick mit '|'. Beispiel: Photoshop|4 <br /> Der Wert der F&auml;higkeit sein kann 0-5.<br /> F&auml;higkeit 1|1, F&auml;higkeit 2|3 etc.");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_2", "F&auml;higkeit 2");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_2_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_3", "F&auml;higkeit 3");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_3_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_4", "F&auml;higkeit 4");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_4_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_5", "F&auml;higkeit 5");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_5_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_6", "F&auml;higkeit 6");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_6_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_7", "F&auml;higkeit 7");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_7_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_8", "F&auml;higkeit 8");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_8_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_9", "F&auml;higkeit 9");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_9_DSC", "");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_10", "F&auml;higkeit 10");
+define("_CO_PORTFOLIO_INDEXPAGE_INDEX_SKILLS_10_DSC", "");
+
+// some general constants
+if(!defined("_ER_UP_UNKNOWNFILETYPEREJECTED")) define("_ER_UP_UNKNOWNFILETYPEREJECTED", "Dateityp unbekannt.");
+define("_CO_PORTFOLIO_PREVIEW", "Vorschau");
+define("_CO_PORTFOLIO_EDIT", "Bearbeiten");
+if(!defined("_CO_SUBMIT")) define("_CO_SUBMIT", "Einreichen");
+define("_CO_PORTFOLIO_DELETE", "L&ouml;schen");
+define("_CO_PORTFOLIO_VIEW", "Ansehen");
+/**
+ * added in 1.1
+ */
+define("_CO_PORTFOLIO_PORTFOLIO_CONTACT_SBJ", "Neue Nachricht in Portfolios");
+define("_CO_PORTFOLIO_PORTFOLIO_CONTACT_BDY", "Sie haben eine neue Nachricht aus dem Portfolio Module. Gehe zu &raquo;%s&laquo; um die Nachricht zu lesen");
---
+++ portfolio/language/german/modinfo.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * 'Portfolio' is an portfolio management module for ImpressCMS
+ *
+ * File: /language/german/modinfo.php
+ * 
+ * modinfo language file
+ * 
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ * 				Portfolio
+ * @since		1.00
+ * @author		QM-B <qm-b@hotmail.de>
+ * @version		$Id$
+ * @package		portfolio
+ *
+ */
+
+defined("ICMS_ROOT_PATH") or die("ImpressCMS Basispfad wurde nicht definiert");
+
+define("_MI_PORTFOLIO_NAME", "Portfolio");
+define("_MI_PORTFOLIO_DSC", "'Portfolio' ist ein Referenzobjekt Management Modul f&uuml;r ImpressCMS");
+
+// templates
+define("_MI_PORTFOLIO_INDEX_TPL", "&Uuml;bersicht");
+define("_MI_PORTFOLIO_PORTFOLIO_TPL", "Einzel-Portfolio");
+define("_MI_PORTFOLIO_CONTACT_TPL", "Liste der Meldungen im Frontend");
+define("_MI_PORTFOLIO_HEADER_TPL", "Header included in frontend");
+define("_MI_PORTFOLIO_FOOTER_TPL", "Footer included in frontend");
+define("_MI_PORTFOLIO_ADMIN_TPL", "Admin-Vorlage(Template)");
+define("_MI_PORTFOLIO_REQUIREMENTS_TPL", "Anforderungen sind zu �berpr�fen");
+define("_MI_PORTFOLIO_SPORTFOLIO_TPL", "einzigen Portfolio in Schleife");
+define("_MI_PORTFOLIO_SCAT_TPL", "einzelne CAT in einer Schleife");
+
+// blocks
+define("_MI_PORTFOLIO_BLOCK_RECENT_PORTFOLIOS", "Aktuelle Referenzobjekte");
+define("_MI_PORTFOLIO_BLOCK_RECENT_PORTFOLIOS_DSC", "Anzeige j&uuml;ngsten Referenzpobjekte");
+define("_MI_PORTFOLIO_BLOCK_RECENT_CONTACTS", "Aktuelle Nachrichten");
+define("_MI_PORTFOLIO_BLOCK_RECENT_CONTACTS_DSC", "Anzeige j&uml;ngsten Kontakte in einem Block");
+define("_MI_PORTFOLIO_BLOCK_CATEGORIES", "Kategorie Men&uuml;");
+define("_MI_PORTFOLIO_BLOCK_CATEGORIES_DSC", "Kategorie Liste");
+define("_MI_PORTFOLIO_BLOCK_SPOTLIGHT", "Spotlight");
+define("_MI_PORTFOLIO_BLOCK_SPOTLIGHT_DSC", "Spotlight Block");
+define("_MI_PORTFOLIO_BLOCK_RANDOM_PORTFOLIOS", "zuf&auml;lliges Referenzobjekt");
+define("_MI_PORTFOLIO_BLOCK_RANDOM_PORTFOLIOS_DSC", "zuf&auml;lliger Block");
+
+//config
+define("_MI_PORTFOLIO_DATE_FORMAT", "Datumsformat");
+define("_MI_PORTFOLIO_DATE_FORMAT_DSC", "");
+define("_MI_PORTFOLIO_SHOW_BREADCRUMBS", "Zeige Breadcrumb Navigation");
+define("_MI_PORTFOLIO_SHOW_BREADCRUMBS_DSC", "");
+define("_MI_PORTFOLIO_THUMBNAIL_WIDTH", "Vorschaubild Breite");
+define("_MI_PORTFOLIO_THUMBNAIL_WIDTH_DSC", "Breite des Vorschaubildes in der �bersicht");
+define("_MI_PORTFOLIO_THUMBNAIL_HEIGHT", "Vorschaubild H&ouml;he");
+define("_MI_PORTFOLIO_THUMBNAIL_HEIGHT_DSC", "H&ouml;he des Vorschaubildes in der �bersicht");
+define("_MI_PORTFOLIO_DISPLAY_WIDTH", "Bildanzeige Breite");
+define("_MI_PORTFOLIO_DISPLAY_WIDTH_DSC", "Bildanzeige Breite auf Modul-und einzelnen Portfolio-Ansicht");
+define("_MI_PORTFOLIO_DISPLAY_HEIGHT", "Bildanzeige H&ouml;he");
+define("_MI_PORTFOLIO_DISPLAY_HEIGHT_DSC", "Bildanzeige H&ouml;he auf Modul-und einzelnen Portfolio-Ansicht");
+define("_MI_PORTFOLIO_IMAGE_UPLOAD_WIDTH", "Bild-Upload Breite");
+define("_MI_PORTFOLIO_IMAGE_UPLOAD_WIDTH_DSC", "");
+define("_MI_PORTFOLIO_IMAGE_UPLOAD_HEIGHT", "Bild-Upload H&ouml;he");
+define("_MI_PORTFOLIO_IMAGE_UPLOAD_HEIGHT_DSC", "");
+define("_MI_PORTFOLIO_IMAGE_FILE_SIZE", "Maximale Bildgr&ouml;&szlig;e");
+define("_MI_PORTFOLIO_IMAGE_FILE_SIZE_DSC", "");
+define("_MI_PORTFOLIO_SHOW_PORTFOLIO_COLUMNS", "Wie viele Spalten sollen auf der Modul-Index Seiteangezeigt werden");
+define("_MI_PORTFOLIO_SHOW_PORTFOLIO_COLUMNS_DSC", "");
+define("_MI_PORTFOLIO_SHOW_PORTFOLIO_ROWS", "Wie viele Zeilen sollen auf der Modul-Index Seiteangezeigt werden");
+define("_MI_PORTFOLIO_SHOW_PORTFOLIO_ROWS_DSC", "");
+define("_MI_PORTFOLIO_USE_RSS", "RSS-Feeds einschalten?");
+define("_MI_PORTFOLIO_USE_RSS_DSC", "");
+define("_MI_PORTFOLIO_USE_ALBUM", "Verwenden Album");
+define("_MI_PORTFOLIO_USE_ALBUM_DSC", "Sie k�nnen ein Album verwenden um mehr als ein Bild,in Ihren Referenzobjekt anzuzeigen");
+define("_MI_PORTFOLIO_GUEST_CAN_CONTACT", "Erlauben G&auml;sten, Ihnen Nachrichten zu senden?");
+define("_MI_PORTFOLIO_GUEST_CAN_CONTACT_DSC", "");
+define("_MI_PORTFOLIO_DEFAULT", "Standard");
+define("_MI_PORTFOLIO_HORIZONTAL", "horizontale Ausrichtung");
+define("_MI_PORTFOLIO_VERTICAL", "vertikale Ausrichtung");
+define("_MI_PORTFOLIO_DISPLAY_TWITTER", "Twitter Button anzeigen?");
+define("_MI_PORTFOLIO_DISPLAY_TWITTER_DSC", "");
+define("_MI_PORTFOLIO_DISPLAY_FBLIKE", "Facebook Button anzeigen?");
+define("_MI_PORTFOLIO_DISPLAY_FBLIKE_DSC", "");
+define("_MI_PORTFOLIO_DISPLAY_GPLUS", "G+ Button anzeigen?");
+define("_MI_PORTFOLIO_DISPLAY_GPLUS_DSC", "");
+
+// ACP Menu
+define("_MI_PORTFOLIO_MENU_PORTFOLIO", "Referenzobjekte");
+define("_MI_PORTFOLIO_MENU_CATEGORY", "Kategorien verwalten");
+define("_MI_PORTFOLIO_MENU_CONTACT", "Nachrichten ansehen");
+define("_MI_PORTFOLIO_MENU_INDEXPAGE", "Bearbeiten der Modul Indexseite");
+define("_MI_PORTFOLIO_MENU_TEMPLATES", "Vorlagen");
+define("_MI_PORTFOLIO_MENU_IMPORT", "Import");
+// Submenu
+define("_MI_PORTFOLIO_PORTFOLIO_EDIT", "Bearbeiten Sie Ihre Portfolio");
+define("_MI_PORTFOLIO_PORTFOLIO_CREATINGNEW", "Absenden neue Portfolio");
+define("_MI_PORTFOLIO_CATEGORY_EDIT", "Bearbeiten Sie Ihre Kategorien");
+define("_MI_PORTFOLIO_CATEGORY_CREATINGNEW", "Erstellen Sie eine neue Kategorie");
+define("_MI_PORTFOLIO_INDEXPAGE_EDIT", "Bearbeiten der Modul Indexseite");
+/**
+ * added in 1.1
+ */
+define("_MI_PORTFOLIO_DEFAULT_STARTPAGE", "Legen Sie die Startseite des Moduls fest");
+define("_MI_PORTFOLIO_DEFAULT_STARTPAGE_DSC", "");
+define("_MI_PORTFOLIO_DEFAULT_STARTPAGE_DEF", "Standard Modul-Startseite");
+define("_MI_PORTFOLIO_DEFAULT_STARTPAGE_CAT", "Kategorienübersicht");
