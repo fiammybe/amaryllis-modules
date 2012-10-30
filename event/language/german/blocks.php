883db19d9c0ac4a71484810c596cdd7aa1d7defe
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-10-30 10:53:54 +0000

    updated version status added german language files - thanks to optimistdd

---
+++ event/language/german/blocks.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 'Event' is an event/event module for ImpressCMS, which can display google events, too
+ *
+ * File: /language/german/blocks.php
+ * 
+ * german blocks language file
+ * 
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ * 				Event
+ * @since		1.00
+ * @author		optimistdd
+ * @version		$Id$
+ * @package		event
+ *
+ */
+
+defined("ICMS_ROOT_PATH") or die("ImpressCMS Basispfad wurde nicht definiert");
+
+define("_MB_EVENT_TIMERANGE", "W&auml;hlen Sie den Zeitbereich der angezeigt werden soll");
+define("_MB_EVENT_CURRENT_DAY", "Aktueller Tag");
+define("_MB_EVENT_CURRENT_WEEK", "Aktuelle Woche");
+define("_MB_EVENT_CURRENT_MONTH", "Aktueller Monat");
+define("_MB_EVENT_NO_EVENTS", "Keine Termine f�r den ausgew�hlten Bereich gefunden.");
+define("_MB_EVENT_START", "Startdatum");
+define("_MB_EVENT_END", "Enddatum");
+define("_MB_EVENT_CATEGORY", "Kategorie");
+define("_MB_EVENT_CITY", "Stadt");
+define("_MB_EVENT_PLZ", "Postleitzahl");
+define("_MB_EVENT_DISPLAY_CITY", "Suche nach Stadt?");
+define("_MB_EVENT_DISPLAY_PLZ", "Suche nach  Postleitzahl?");
