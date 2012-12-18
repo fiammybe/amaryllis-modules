8f9c2575aad4643544029eedf73836fe4f943623
info <info@pmp-webdesign.de>
2012-12-18 08:24:42 +0000

    Neue Datei

---
+++ visitorvoice/language/german/modinfo.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * English language constants related to module information
+ *
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2011
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		QM-B <qm-b@hotmail.de>
+ * @package		visitorvoice
+ * @version		$Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+define("_MI_VISITORVOICE_NAME", "Kundenmeinungen");
+define("_MI_VISITORVOICE_DSC", "Einfaches Kundenmeinungsmodul f&Uuml;r ImpressCMS ");
+// Templates
+define("_MI_VISITORVOICE_VISITORVOICE_TPL", "Kundenmeinung Index Seite");
+define("_MI_VISITORVOICE_ADMIN_TPL", "Kundenmeinung admin Template");
+define("_MI_VISITORVOICE_REQUIREMENTS_TPL", "Kundenmeinung Anforderungen");
+// Blocks
+define("_MI_VISITORVOICE_BLOCK_RECENT_ENTRIES", "Letzte Eintr&auml;ge");
+define("_MI_VISITORVOICE_BLOCK_RECENT_ENTRIES_DSC", "Letzte Eintr&auml;ge im Block anzeigen");
+// Config
+define("_MI_VISITORVOICE_DATE_FORMAT", "Datumsformat");
+define("_MI_VISITORVOICE_DATE_FORMAT_DSC", "Sie k&ouml;nnen das Datumsformat einstellen wie Sie es w&uuml;nschen");
+define("_MI_VISITORVOICE_SHOW_BREADCRUMBS", "Zeige die Breadcrumb Navigation");
+define("_MI_VISITORVOICE_SHOW_BREADCRUMBS_DSC", "zeigen/ausblenden der Breadcrumb Navigation in der Useransicht");
+define("_MI_VISITORVOICE_SHOW_ENTRIES", "Wie viele Eintr&auml;ge sollten in Useransicht angezeigt werden?");
+define("_MI_VISITORVOICE_SHOW_ENTRIES_DSC", "");
+define("_MI_VISITORVOICE_USE_MODERATION", "Verwenden Moderation?");
+define("_MI_VISITORVOICE_USE_MODERATION_DSC", "M&ouml;chten Sie die Eintr&auml;ge moderieren?");
+define("_MI_VISITORVOICE_SHOW_AVATAR", "Zeige Avatar in Frontend");
+define("_MI_VISITORVOICE_SHOW_AVATAR_DSC", "Dadurch werden die Benutzer Avatare");
+define("_MI_VISITORVOICE_NEEDS_APPROVAL", "Genehmigen Eintr&auml;ge, bevor sie ver&ouml;ffentlicht sind?");
+define("_MI_VISITORVOICE_NEEDS_APPROVAL_DSC", "");
+define("_MI_VISITORVOICE_ALLOW_IMAGEUPLOAD", "M&ouml;chten Sie erm&ouml;glichen Bild hochladen?");
+define("_MI_VISITORVOICE_ALLOW_IMAGEUPLOAD_DSC", "");
+define("_MI_VISITORVOICE_IMAGE_UPLOAD_WIDTH", "Max Image Upload Breite");
+define("_MI_VISITORVOICE_IMAGE_UPLOAD_WIDTH_DSC", "");
+define("_MI_VISITORVOICE_IMAGE_UPLOAD_HEIGHT", "Max Image Upload H&ouml;he");
+define("_MI_VISITORVOICE_IMAGE_UPLOAD_HEIGHT_DSC", "");
+define("_MI_VISITORVOICE_IMAGE_FILE_SIZE", "Maximal zul&auml;ssige Upload Gr&ouml;ße");
+define("_MI_VISITORVOICE_IMAGE_FILE_SIZE_DSC", "");
+define("_MI_VISITORVOICE_THUMBNAIL_WIDTH", "Miniaturbild Breite");
+define("_MI_VISITORVOICE_THUMBNAIL_WIDTH_DSC", "Breite des Miniaturbild zur Anzeige hochgeladenen Bilder");
+define("_MI_VISITORVOICE_THUMBNAIL_HEIGHT", "Miniaturbild H&ouml;he");
+define("_MI_VISITORVOICE_THUMBNAIL_HEIGHT_DSC", "H&ouml;he des Miniaturbild zur Anzeige hochgeladenen Bilder");
+define("_MI_VISITORVOICE_GUEST_CAN_SUBMIT", "Die G&auml;ste k&ouml;nnen einzureichen neue Eintr&auml;ge?");
+define("_MI_VISITORVOICE_GUEST_CAN_SUBMIT_DSC", "M&ouml;gen Sie, damit die G&auml;ste, um neue Eintr&auml;ge einreichen, set'YES '");
+define("_MI_VISITORVOICE_SHOW_EMAIL", "Anzeigen der E-Mail Adresse im Frontend?");
+define("_MI_VISITORVOICE_SHOW_EMAIL_DSC", "zeigen/ausblenden der E-Mail Adresse im fronted");
+define("_MI_VISITORVOICE_DISPLAY_EMAIL", "Wie die E-Mail-Adresse im Frontend angezeigt werden?");
+define("_MI_VISITORVOICE_DISPLAY_EMAIL_DSC", "");
+define("_MI_VISITORVOICE_DISPLAY_MAIL_SPAMPROT", "ext Spam ohne &uuml;berpr&uuml;fung Liste der verbotenen gesch&uuml;tzt");
+define("_MI_VISITORVOICE_DISPLAY_MAIL_IMGPROT", "gewohnten E-Mail ohne Pr&uuml;fung Sperrlst");
+define("_MI_VISITORVOICE_DISPLAY_MAIL_SPAMPROT_BANNED", "Text-Spam mit der &uuml;berpr&uuml;fung Liste der verbotenen gesch&uuml;tzt");
+define("_MI_VISITORVOICE_DISPLAY_MAIL_IMGPROT_BANNED", "gewohnten E-Mail mit der &uuml;berpr&uuml;fung Liste der verbotenen");
+// ACP menu
+define("_MI_VISITORVOICE_MENU_VISITORVOICE", "Kundenmeinungen");
+define("_MI_VISITORVOICE_MENU_INDEXPAGE", "Edit Indexpage");
+define("_MI_VISITORVOICE_MENU_TEMPLATES", "Templates");
+define("_MI_VISITORVOIVE_MENU_IMPORT", "Import");
+
+
