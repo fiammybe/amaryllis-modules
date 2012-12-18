cf0592dc5eca4607b1a6495bc16d9e4f64d975b0
info <info@pmp-webdesign.de>
2012-12-18 09:52:20 +0000

    file updated

--- career/language/german/main.php
+++ career/language/german/main.php
@@ -2,36 +2,40 @@
 /**
  * 'Career' is an career management module for ImpressCMS
  *
- * File: /language/german/admin.php
+ * File: /language/german/main.php
  * 
- * admin language constants
+ * main language constants
  * 
- * @copyright    Copyright QM-B (Steffen Flohrer) 2012
- * @license        http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * ----------------------------------------------------------------------------------------------------------
- *                 Career
- * @since        1.00
- * @author        QM-B <qm-b@hotmail.de>
- * @version        $Id$
- * @package        career
+ * 				Career
+ * @since		1.00
+ * @author		QM-B <qm-b@hotmail.de>
+ * @version		$Id$
+ * @package		career
  *
  */
 
-if (!defined("ICMS_ROOT_PATH")) die("ImpressCMS Basispfad wurde nicht definiert");
+ if (!defined("ICMS_ROOT_PATH")) die("ImpressCMS Basispfad wurde nicht definiert");
 
-//general used constants
-define("_AM_CAREER_OFFLINE", "Offline");
-define("_AM_CAREER_ONLINE", "Online");
-define("_AM_CAREER_WEIGHTS_UPDATED", "Reihenfolge erfolgreich aktualisiert");
-
-// constants for /admin/career.php
-define("_AM_CAREER_CREATED", "Stellenanzeigen erfolgreich erstellt.");
-define("_AM_CAREER_MODIFIED", "Stellenanzeigen erfolgreich geändert.");
-define("_AM_CAREER_CAREER_ADD", "Fügen Sie ein neue Stellenanzeigen hinzu");
-//constants for /admin/department.php
-define("_AM_CAREER_DEPARTMENT_CREATED", "Arbeitsbereich erfolgreich erstellt.");
-define("_AM_CAREER_DEPARTMENT_MODIFIED", "Arbeitsbereicherfolgreich geändert.");
-define("_AM_CAREER_DEPARTMENT_ADD", "Fügen Sie einen neuen Arbeitsbereich hinzu");
-// constants for admin/indexpage.php
-define("_AM_CAREER_INDEXPAGE_MODIFIED", "Indexseite wurde erfolgreich geändert ");
-define("_AM_CAREER_INDEXPAGE_EDIT", "Bearbeiten der Indexseite");
+// general used constants
+define("_MD_CAREER_ADMIN_PAGE", ":: Admin page ::");
+define("_MD_CAREER_MESSAGES_LINK", " Nachrichten ::");
+define("_MD_CAREER_MESSAGE", "senden Anwendung");
+define("_MD_CAREER_MAILTO", "Beratung Jobangebot");
+define("_MD_CAREER_MESSAGES_BC", "verwalten von Nachrichten");
+define("_MD_CAREER_VCARD", "Kontakt Informationen");
+define("_MD_CAREER_CAREERLIST", "Stellenangebote für diese Abteilung");
+define("_MD_CAREER_NO_CAREERS", "Leider gibt es derzeit keine Angebote für diese Abteilung.");
+define("_MD_CAREER_CAREER_NOT_FOUND", "Leider wurde der angeforderte Karriere nicht gefunden.");
+define("_MD_CAREER_MAILTO_SBJ", "I%20found%20a%20great%20Job%20Offering%20for%20you Ich fand große Job-Angebot für Sie");
+define("_MD_CAREER_MAILTO_BDY", "I%20found%20a%20great%20Job%20Offering%20for%20you Ich fand große Job-Angebot für Sie");
+define("_MD_CAREER_PDF", "Holen PDF");
+define("_MD_CAREER_PRINT", "Drucken");
+define("_MD_CAREER_PUBLISHER", "Verfasst von");
+define("_MD_CAREER_CATS", "Bereich Abteilung:");
+define("_MD_CAREER_UPDATED_ON", "Aktualisiert am");
+define("_MD_CAREER_PUBLISHED_ON", "Veröffentlicht am");
+define("_MD_CAREER_UPDATER", "Zuletzt geändert von");
+define("_THANKS_SUBMISSION_REV", "Vielen Dank für Ihre Bewerbung. Wir werden Uns so schnell wie möglich mit Ihnen in Verbindung setzen.");
