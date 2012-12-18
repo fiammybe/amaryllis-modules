4e817d49a5d3bac70587107952f44d0bf192fdb0
info@pmp-webdesign.de <info@pmp-webdesign.de@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-12-18 09:42:19 +0000

    New File

---
+++ trunk/career/language/german/blocks.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 'Career' is an career management module for ImpressCMS
+ *
+ * File: /language/german/blocks.php
+ * 
+ * block language constants
+ * 
+ * @copyright    Copyright QM-B (Steffen Flohrer) 2012
+ * @license        http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ *                 Career
+ * @since        1.00
+ * @author        QM-B <qm-b@hotmail.de>
+ * @version        $Id$
+ * @package        career
+ *
+ */
+
+if (!defined("ICMS_ROOT_PATH")) die("ImpressCMS Basispfad wurde nicht definiert");
+
+// Department block
+define("_MB_CAREER_DEPARTMENT_SORT", "<b>Sortieren:</b>");
+define("_MB_CAREER_DEPARTMENT_ORDER", "<b>Auftrag:</b>");
+define("_MB_CAREER_DEPARTMENT_SELECT", "Wählen Sie, für welche Abteilung die Karrieren angezeigt werden soll");
+define("_MB_CAREER_CAREER_RECENT_LIMIT", "begrenzen");
+
