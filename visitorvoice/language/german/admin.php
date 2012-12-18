8c1c12cb8c08a2d0e2bb1bf6d320db91bc2d430d
info <info@pmp-webdesign.de>
2012-12-18 08:22:46 +0000

    Datei updated

--- visitorvoice/language/german/admin.php
+++ visitorvoice/language/german/admin.php
@@ -1,1 +1,37 @@
+<?php
+/**
+ * German language constants used in admin section of the module
+ *
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2011
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		QM-B <qm-b@hotmail.de>
+ * @package		visitorvoice
+ * @version		$Id$
+ */
 
+defined("ICMS_ROOT_PATH") or die("ICMS root path ist nicht definiert");
+// general in use
+define("_AM_VISITORVOICE_EDIT", "Bearbeiten");
+define("_AM_VISITORVOICE_MODIFIED", "Modifizieren");
+
+// Requirements
+define("_AM_VISITORVOICE_REQUIREMENTS", "VisitorVoice Richtlinien");
+define("_AM_VISITORVOICE_REQUIREMENTS_INFO", "Wir haben Ihr System überpr&uuml;ft, leider erf&uuml;llt es nicht alle Anforderungen die VisitorVoice Modul ben&ouml;tigt. Hier sind die Anforderungen die das Modul ben&ouml;tigt.");
+define("_AM_VISITORVOICE_REQUIREMENTS_ICMS_BUILD", "VisitorVoice ben&ouml;tigt ImpressCMS 1.3");
+define("_AM_VISITORVOICE_REQUIREMENTS_SUPPORT", "Sollten Sie irgendwelche Fragen haben oder Anregungen, besuchen Sie bitte unsere Foren auf<a href='http://community.impresscms.org'>http://community.impresscms.org</a>.");
+
+// Visitorvoice
+define("_AM_VISITORVOICE_VISITORVOICES", "Kundenmeinungen");
+define("_AM_VISITORVOICE_VISITORVOICES_DSC", "Alle Kundenmeinungen im Modul");
+define("_AM_VISITORVOICE_VISITORVOICE_CREATE", "Neue Kundenmeinung");
+define("_AM_VISITORVOICE_VISITORVOICE", "Kundenmeinung");
+define("_AM_VISITORVOICE_VISITORVOICE_CREATE_INFO", "Bitte f&uuml;llen Sie das Formular aus um eine neue Kundenmeinung zu erstellen.");
+define("_AM_VISITORVOICE_VISITORVOICE_EDIT", "Bearbeiten der Kundenmeinung");
+define("_AM_VISITORVOICE_VISITORVOICE_EDIT_INFO", "Bitte f&uuml;llen Sie das Formular aus um eine neue Kundenmeinung zu bearbeiten.");
+define("_AM_VISITORVOICE_VISITORVOICE_MODIFIED", "Die Kundenmeinung wurde erfolgreich ge&uuml;ndert.");
+define("_AM_VISITORVOICE_VISITORVOICE_CREATED", "Die Kundenmeinung wurde erfolgreich erstellt.");
+define("_AM_VISITORVOICE_VISITORVOICE_VIEW", "Kundenmeinung Info");
+define("_AM_VISITORVOICE_VISITORVOICE_VIEW_DSC", "Alle Information über VisitorVoice.");
+define("_AM_VISITORVOICE_APPROVE_FALSE", "Genehmigt");
+define("_AM_VISITORVOICE_APPROVE_TRUE", "Verweigert");
