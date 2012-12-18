7736f658c55195d183cabdd553d6685d1988678f
info@pmp-webdesign.de <info@pmp-webdesign.de@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-12-18 09:31:17 +0000

    Neue Datei

---
+++ trunk/visitorvoice/language/german/blocks.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* German language constants used in blocks of the module
+*
+* @copyright	Copyright QM-B (Steffen Flohrer) 2011
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		QM-B <qm-b@hotmail.de>
+* @package		visitorvoice
+* @version		$Id$
+*/
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path ist nicht definiert");
+
+define("_MB_VISITORVOICE_VISITORVOICE_RECENT_LIMIT", "Eintr&auml;ge anzeigen:");
+define("_MB_VISITORVOICE_VIEWLIST", "Nur Liste anzeigen?");
+define("_MB_VISITORVOICE_READ_MORE", "Read more...");
