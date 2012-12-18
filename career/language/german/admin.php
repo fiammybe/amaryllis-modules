72298dae6d62ce4496cea3c5d4c0a61f0ed3c755
info <info@pmp-webdesign.de>
2012-12-18 09:49:27 +0000

    New File

---
+++ career/language/german/admin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * 'Career' is an career management module for ImpressCMS
+ *
+ * File: /language/german/admin.php
+ * 
+ * admin language constants
+ * 
+ * @copyright    Copyright QM-B (Steffen Flohrer) 2012
+ * @license        http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ *                 Career
+ * @since        1.00
+ * @author        QM-B <qm-b@hotmail.de>
+ * @version        $Id$
+ * @package        career
+ *
+ */
+
+if (!defined("ICMS_ROOT_PATH")) die("ImpressCMS Basispfad wurde nicht definiert");
+
+//general used constants
+define("_AM_CAREER_OFFLINE", "Offline");
+define("_AM_CAREER_ONLINE", "Online");
+define("_AM_CAREER_WEIGHTS_UPDATED", "Reihenfolge erfolgreich aktualisiert");
+
+// constants for /admin/career.php
+define("_AM_CAREER_CREATED", "Stellenanzeigen erfolgreich erstellt.");
+define("_AM_CAREER_MODIFIED", "Stellenanzeigen erfolgreich geändert.");
+define("_AM_CAREER_CAREER_ADD", "Fügen Sie ein neue Stellenanzeigen hinzu");
+//constants for /admin/department.php
+define("_AM_CAREER_DEPARTMENT_CREATED", "Arbeitsbereich erfolgreich erstellt.");
+define("_AM_CAREER_DEPARTMENT_MODIFIED", "Arbeitsbereicherfolgreich geändert.");
+define("_AM_CAREER_DEPARTMENT_ADD", "Fügen Sie einen neuen Arbeitsbereich hinzu");
+// constants for admin/indexpage.php
+define("_AM_CAREER_INDEXPAGE_MODIFIED", "Indexseite wurde erfolgreich geändert ");
+define("_AM_CAREER_INDEXPAGE_EDIT", "Bearbeiten der Indexseite");
