3b67d839dc8145f3c90fca8d5e3d3ec1985952d5
info@pmp-webdesign.de <info@pmp-webdesign.de@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2012-12-18 09:36:02 +0000

    Neue Datei

---
+++ trunk/visitorvoice/language/german/main.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * German language constants used in the user side of the module
+ *
+ * @copyright    Copyright QM-B (Steffen Flohrer) 2011
+ * @license        http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since        1.0
+ * @author        QM-B <qm-b@hotmail.de>
+ * @package        visitorvoice
+ * @version        $Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path ist nicht definiert");
+
+define("_MD_VISITORVOICE_ADMIN_PAGE", ":: Admin page ::");
+
+define("_MD_VISITORVOICE_CREATE", "Eine Neue Kundenmeinung erstellen");
+define("_MD_VISITORVOICE_ADD_ENTRY", "Neue Kundenmeinung erstellen");
+define("_MD_VISITORVOICE_REPLY", "Antworten");
+define("_MD_VISITORVOICE_PUBLISHED_BY", "Ver&ouml;ffentlicht von");
+define("_MD_VISITORVOICE_PUBLISHED_ON", "Ver&ouml;ffentlicht am");
+define("_MD_VISITORVOICE_PERM", "Sorry, sie haben keine Berechtigungen");
+define("_MD_VISITORVOICE_PERMISSION", "Sie haben keine Berechtigungen. Bitte einloggen Sie sich ein  oder registrieren Sie sich, um einen neuen Beitrag einzureichen.");
+define("_MD_VISITORVOICE_CREATED", "Kundenmeinung Eintrag erstellt, danke.");
+define("_MD_VISITORVOICE_MODIFIED", "Kundenmeinung Eintrag, modifiziert.");
+define("_MD_VISITORVOICE_MAIL_CONTACT", "Kontakt");
