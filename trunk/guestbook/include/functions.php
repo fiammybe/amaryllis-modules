eb29d83ffc332b9190faf0198e03b29d850001af
st.flohrer <st.flohrer@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2013-04-24 08:16:12 +0000

    ongoing dev process of guestbook 1.2

---
+++ trunk/guestbook/include/functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 'Guestbook' is a small, light weight guestbook module for ImpressCMS
+ *
+ * File: /submit.php
+ * 
+ * submit entries
+ * 
+ * @copyright	Copyright QM-B (Steffen Flohrer) 2012
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * ----------------------------------------------------------------------------------------------------------
+ * 				Guestbook
+ * @since		1.00
+ * @author		QM-B <qm-b@hotmail.de>
+ * @version		$Id: functions.php 86 2013-02-05 00:20:08Z qm-b $
+ * @package		guestbook
+ *
+ */
+
+function writeJsonRequest($content) {
+	$path = ICMS_TRUST_PATH.'/modules/'.GUESTBOOK_DIRNAME.'/cache';
+	if(!is_dir($path)) mkdir($path, 0777, TRUE);
+	$filename = $path.'/guestbook_cache.json';
+    if(!$file = fopen($filename,"w")) {
+        echo 'failed open file';
+        return FALSE;
+    }
+    if(fwrite($file, var_export($content, TRUE)) == - 1) {
+        echo 'failed write file';
+        return FALSE;
+    }
+    fclose ($file);
+    return TRUE;
+}
