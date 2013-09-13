653049bdb6560dbb9747f320f80ea4ad32e5292b
impresscms@gmail.com <impresscms@gmail.com@66ca2e0c-c8bd-fb7b-0ff6-659cc54a85ad>
2013-09-13 09:01:38 +0000

    Typofix

--- trunk/icmspoll/language/english/common.php
+++ trunk/icmspoll/language/english/common.php
@@ -117,3 +117,4 @@ define("_CO_ICMSPOLL_OPTION_TOTALVOTES", "votes");
 define("_CO_ICMSPOLL_ADMIN_SHOW_DETAILS", "Show more informations");
 define("_CO_ICMSPOLL_PRESENT_BY_USERPROFILE", "This Poll was created by");
 define("_CO_ICMSPOLL_POLLS_VISIT_USERPROFILE", "Visit the profile");
+define("_CO_ICMSPOLL_POLLS_VOTES_UNTIL_NOW", "Until now votes");
